8b30cc8e5f26ab7121cbf13bb1059aac0d10a775
Saeed Nemati <sn.1361@gmail.com>
2021-06-21 21:36:36 +0430

    enum

---
+++ Business/EnumItem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Holism.Business
+{
+    public class EnumItem
+    {
+        public long Id { get; set; }
+
+        public Guid Guid { get; set; }
+
+        public string Key { get; set; }
+    }
+}

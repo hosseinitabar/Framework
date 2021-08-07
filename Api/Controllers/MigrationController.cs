1ed1320a7c2dedd541673bc65fd9b0bf5bd5eac2
Saeed Nemati <sn.1361@gmail.com>
2021-08-07 15:20:28 +0430

    committed by script

---
+++ Api/Controllers/MigrationController.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using System;
+using System.Reflection;
+using Holism.DataAccess;
+using Holism.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Holism.Api
+{
+    public class MigrationController : DefaultController
+    {
+        [HttpGet]
+        public IActionResult Apply()
+        {
+            var assemblies = Directory
+                .GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
+                .Select(x => Assembly.Load(AssemblyName.GetAssemblyName(x)));
+            assemblies = assemblies
+                .Where(i => i.GetName().Name.Contains("DataAccess"))
+                .ToList();
+            var result = "";
+            foreach (var assembly in assemblies) 
+            {
+                var types = assembly
+                    .GetTypes()
+                    .Where(i => i.Name.Contains("Context"))
+                    .Where(i => !i.Name.Contains("Snapshot"))
+                    .Where(i => i.Name != "DatabaseContext")
+                    .ToList();
+                foreach (var type in types) 
+                {
+                    result += $"{type.Name}\n";
+                    var context = (DatabaseContext)Activator.CreateInstance(type);
+                    result += context.Database.GetDbConnection().ConnectionString;
+                    context.Database.Migrate();
+                }
+            }
+            return OkJson(result);
+        }
+    }
+}

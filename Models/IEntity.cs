df9ba1e71411687d09470eaae0b34eeaf3cb7402
Saeed Nemati <sn.1361@gmail.com>
2021-08-12 17:03:43 +0430

    committed by script

--- Models/IEntity.cs
+++ Models/IEntity.cs
@@ -1,7 +1,14 @@
-﻿namespace Holism.Models
+﻿using System;
+
+namespace Holism.Models
 {
     public interface IEntity
     {
+        public IEntity()
+        {
+            RelatedItems = new System.Dynamic.ExpandoObject();
+        }
+
         long Id { get; set; }
 
         dynamic RelatedItems { get; set; }

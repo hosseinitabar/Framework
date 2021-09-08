10774667cd0edc52e24b2d0bb9b0894b49b4a006
Saeed Nemati <sn.1361@gmail.com>
2021-09-08 15:07:43 +0430

    committed by script

---
+++ DataAccessGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Holism.DataAccessGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ModelGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Holism.ModelGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}

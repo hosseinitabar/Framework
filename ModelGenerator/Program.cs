caf3c96f6b2e78b59864f237f7494fdbdfb39a01
Saeed Nemati <sn.1361@gmail.com>
2021-09-09 18:25:43 +0430

    committed by script

--- ModelGenerator/Program.cs
+++ ModelGenerator/Program.cs
@@ -8,12 +8,12 @@ namespace Holism.ModelGenerator
     {
         public static void Main(string[] args)
         {
-            args = [
+            args = new string[] {
                 "/HolismDotNet/Ticketing",
                 "HolismDotNet",
                 "Holism",
                 "Ticketing"
-            ];
+            };
             if (args.Length != 4)
             {
                 Logger.LogWarning("Wrong arguments are specified for the model generator");
